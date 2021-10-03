076814d5895f8ba039291d9440729b6575bf32af
shire-kan <aylv.hgyr50@gmail.com>
2021-10-03 16:47:28 +0900

    テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    encrypted_password    {'000000'}
-    password_confirmation {encrypted_password}
-    first_name            {'名前'}
-    last_name             {'苗字'}
-    first_kana            {'ナマエ'}
-    last_kana             {'ミョウジ'}
-    birthday              {'1930-01-01'}
+    transient do
+      person { Gimei.name }
+    end
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name            {Faker::Japanese::Name.first_name}
+    last_name             {Faker::Japanese::Name.last_name}
+    first_kana            {Faker::Japanese::Name.first_name.yomi}
+    last_kana             {Faker::Japanese::Name.last_name.yomi}
+    birthday              {Faker::Date.birthday}
   end
 end
