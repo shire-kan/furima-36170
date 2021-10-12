a9275f514fe5fbf4e8c746f35ff3c2bbf23df231
shire-kan <aylv.hgyr50@gmail.com>
2021-10-12 13:01:02 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to :category
   belongs_to :condition
   belongs_to :deliverydays
@@ -8,6 +9,27 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  validates :title, :text, presence: true
-  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :title,
+            presence: true
+  validates :content,
+            presence: true
+  validates :category_id,
+            presence: true,
+            numericality: { other_than: 1 }
+  validates :condition_id,
+            presence: true,
+            numericality: { other_than: 1 }
+  validates :method_id,
+            presence: true,
+            numericality: { other_than: 1 }
+  validates :prefecture_id,
+            presence: true,
+            numericality: { other_than: 1 }
+  validates :deliverydays_id,
+            presence: true,
+            numericality: { other_than: 1 }
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price,
+              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
 end
