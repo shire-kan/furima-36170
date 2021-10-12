fb6d2180bcd896da4c7d1b4c55349bff1bebc7e6
shire-kan <aylv.hgyr50@gmail.com>
2021-10-12 10:43:31 +0900

    ActiveHashアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :deliverydays
+  belongs_to :method
+  belongs_to :prefecture
+
   has_one_attached :image
+
+  validates :title, :text, presence: true
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
