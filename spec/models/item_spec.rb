02081329238c5638c62c529dd84a79a178660462
shire-kan <aylv.hgyr50@gmail.com>
2021-10-13 18:53:38 +0900

    コードレビュー2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -133,6 +133,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Deliverydays must be less than or equal to 4')
       end
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end
