b56ad576d1fbd8dd097d2e692f0be730a21f7891
shire-kan <aylv.hgyr50@gmail.com>
2021-10-14 20:41:11 +0900

    一覧ビュー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
 
-  #  def index
-  #    @items = Item.order('created_at DESC')
-  #  end
+    def index
+      @items = Item.all.order(created_at: :DESC)
+    end
 
   def new
     @item = Item.new
@@ -21,7 +21,16 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :content, :price, :condition_id, :shipping_id, :prefecture_id, :deliverydays_id,
-                                 :category_id).merge(user_id: current_user.id)
+    params.require(:item)
+          .permit(:title,
+                  :content,
+                  :price,
+                  :condition_id,
+                  :shipping_id,
+                  :prefecture_id,
+                  :deliverydays_id,
+                  :category_id,
+                  :image)
+          .merge(user_id: current_user.id)
   end
 end
