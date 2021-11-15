e7172e5bc358cbdb028f5f459ad5da682244dd24
shire-kan <aylv.hgyr50@gmail.com>
2021-11-15 12:09:30 +0900

    データベース

---
+++ db/migrate/20211028150159_create_residences.rb
@@ -0,0 +1,13 @@
+class CreateResidences < ActiveRecord::Migration[6.0]
+  def change
+    create_table :residences do |t|
+      t.string      :postal_code,     null: false
+      t.integer     :prefecture_id,   null: false
+      t.string      :municipality,    null: false
+      t.string      :address,         null: false
+      t.string      :building
+      t.string      :phone_number,    null: false
+      t.references  :order,           null: false,    foreign_key: true
+    end
+  end
+end
